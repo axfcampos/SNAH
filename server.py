4956e1b8be7e06c2d3e40a20ac0050d35012725f
Alexandre Campos <alexandrefilipecampos@gmail.com>
2014-04-18 03:33:33 +0100

    making the server

--- server.py
+++ server.py
@@ -9,5 +9,13 @@ t.addChannel("Boot", sys.stdout);
 t.getNode(1).bootAtTime(100001);
 t.getNode(2).bootAtTime(800002);
 
-for i in range(100):
-    t.runNextEvent()
+#create nodes
+
+
+#start prompt for user commands
+
+
+#run next event 4ever
+while 1:
+    time.sleep(0.001); #slow things a bit
+    t.runNextEvent(); #run next event
